26cb4865f7ba2c2fdcd719bc08cc7121f3cdcd5c
James V <james.vanhoke@yahoo.com>
2021-04-08 11:24:19 -0700

    linted

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const sequelize = require('../config/connection');
 
 class User extends Model {
   checkPassword(loginPw) {
-    return await bcrypt.compare(loginPw, this.password);
+    return bcrypt.compare(loginPw, this.password);
   }
 }
 
