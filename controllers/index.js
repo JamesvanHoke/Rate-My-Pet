053f2202b076f7c5ee80dbbac8574318d9659d0a
James V <james.vanhoke@yahoo.com>
2021-04-08 12:40:00 -0700

    homeroute setup

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 
 // Setup our overall router controls
 const apiRoutes = require('./api');
+const homeRoutes= require('./homeRoutes')
 // Require any other routes created
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-// Create a / route
 
 module.exports = router;
