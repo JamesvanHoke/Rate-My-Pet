79914057f34dd4e7e2be075dd5767ea3afd2270c
kylegavinsimone <kylesimone@outlook.com>
2021-04-08 12:40:15 -0700

    setting up tests

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,31 +1,31 @@
-const newFormHandler = async (event) => {
+const newPetHandler = async (event) => {
   event.preventDefault();
 
   const pet = document.querySelector('#pet_name').value.trim();
   const description = document.querySelector('#pet_description').value.trim();
   const owner = document.querySelector('#owner_name').value.trim();
   const user = document.querySelector('#user').value.trim();
+  console.log(pet, description, owner )
+  // if (pet && description && owner) {
+  //   const response = await fetch(`/api/pets`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({ pet, description, owner }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
 
-  if (pet && description) {
-    const response = await fetch(`/api/pets`, {
-      method: 'POST',
-      body: JSON.stringify({ pet, description, owner }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to upload pet');
-    }
-  }
+  //   if (response.ok) {
+  //     document.location.replace('/profile');
+  //   } else {
+  //     alert('Failed to upload pet');
+  //   }
+  // }
 };
 
 document
   .querySelector('.new_pet')
-  .addEventListener('submit', newFormHandler);
+  .addEventListener('submit', newPetHandler);
 
 document
   .querySelector('.pet_name');
