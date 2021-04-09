dfe95474c2d38db33fb93ea225086971f2dd07f4
James V <james.vanhoke@yahoo.com>
2021-04-09 14:04:57 -0700

    comments

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// Route is api/users
 router.post('/', async (req, res) => {
+  /*
+  {
+    "name": "string",
+    "email": "string",
+    "Password": "string"
+  }
+  */
+
   try {
     const userData = await User.create(req.body);
 
