0d11a0adfc190fe5f6aca230ebecaca56e5235cf
James V <james.vanhoke@yahoo.com>
2021-04-09 13:49:09 -0700

    comment post/delete functionality

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,38 +1,45 @@
 const router = require('express').Router();
-// const { Comment, User, Pet } = require('../../models');
-// const withAuth = require('../../utils/auth');
+const { Pet, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newProject = await Project.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
+// Route is /api/comments
 
-//     res.status(200).json(newProject);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+// Posts a comment.
+router.post('/:id', async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      //Pulls the user id off the of the session ID
+      commenter_id: req.session.user_id,
+      //Pulls the pet's id off the req url sent and converts it into a number
+      pet_id: parseInt(req.params.id),
+    });
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const projectData = await Project.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-//     if (!projectData) {
-//       res.status(404).json({ message: 'No project found with this id!' });
-//       return;
-//     }
+// Deletes a comment.
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const petData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        commenter_id: req.session.user_id,
+      },
+    });
 
-//     res.status(200).json(projectData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    if (!petData) {
+      res.status(404).json({ message: 'No comment found with this id.' });
+      return;
+    }
+
+    res.status(200).json(petData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
