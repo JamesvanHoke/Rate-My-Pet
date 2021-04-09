c9666f3dadef3cc08ffe5d4f2d9642905656b744
James V <james.vanhoke@yahoo.com>
2021-04-09 10:39:51 -0700

    added ranking and upload page as well as catch url

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,21 @@
 // TODO: Setup Res.render routes
 
 const router = require('express').Router();
-const { Pet, User } = require('../models');
+const { Pet } = require('../models');
 // const withAuth = require('../utils/auth');
 
 // this is the / route
 
+// Our catchall that sends people to homepage.
+router.get('*', (req, res) => {
+  try {
+    res.redirect('/');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Landing Page
 router.get('/', async (req, res) => {
   try {
     // Most recent updated/created Pet
@@ -37,44 +47,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Gallery Page (allows users to go through the collection of pets that can be rated)
 router.get('/gallery/:id', async (req, res) => {
   try {
     const petData = await Pet.findByPk(req.params.id, {});
 
     const soloPet = petData.get({ plain: true });
 
-    //TODO: Replace the res.send with the res.render when templates exist.
-
-    res.send(soloPet);
-    // res.render('solo', {
-    //   ...soloPet,
-    //   logged_in: req.session.logged_in,
-    // });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
+    res.render('solo', {
+      ...soloPet,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// Login Page
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
@@ -82,7 +71,57 @@ router.get('/login', (req, res) => {
     return;
   }
 
-  res.render('login');
+  res.status(200).render('login');
 });
+
+// Top Ranking Page
+router.get('/ranking', async (req, res) => {
+  try {
+    const top5 = await Pet.findAll({
+      order: [['pet_score', 'DESC']],
+      limit: 5,
+    });
+
+    const top5get = top5.map((data) => data.get({ plain: true }));
+
+    res.render('rankings', {
+      top5get,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/upload', (req, res) => {
+  try {
+    res.render('uploads', {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Unneeded? <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Pet }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
