efc5e3c752bd9c2b03919ede51f4ac9e68ce619c
James V <james.vanhoke@yahoo.com>
2021-04-09 09:51:25 -0700

    linting

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 
 // Setup our overall router controls
 const apiRoutes = require('./api');
-const homeRoutes= require('./homeRoutes')
+const homeRoutes = require('./homeRoutes');
 // Require any other routes created
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
 
 module.exports = router;
