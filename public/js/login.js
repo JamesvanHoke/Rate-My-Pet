ddd17d5b095a31bda8ce4a259d05aec9d5577ae6
kylegavinsimone <kylesimone@outlook.com>
2021-04-09 17:22:25 -0700

    Created logout ability

--- public/js/login.js
+++ public/js/login.js
@@ -47,6 +47,22 @@ const signupFormHandler = async (event) => {
   }
 };
 
+const logout = async (event) => {
+  event.preventDefault();
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
+
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
