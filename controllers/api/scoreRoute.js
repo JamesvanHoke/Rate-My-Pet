cff14260e02f1f87aaeaf13740bb77543082df06
James V <james.vanhoke@yahoo.com>
2021-04-11 23:04:07 -0700

    commented out withAuth to remove lint error

--- controllers/api/scoreRoute.js
+++ controllers/api/scoreRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Pet } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 //withAuth was taken out
 router.get('/', (req, res) => {
   Pet.findAll().then((burrito) => {
