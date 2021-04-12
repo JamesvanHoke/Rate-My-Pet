4350e577ac2983ac10e7651ed5c0d86f242b1f78
James V <james.vanhoke@yahoo.com>
2021-04-12 11:14:21 -0700

    score wip

--- public/js/score.js
+++ public/js/score.js
@@ -1,46 +1,55 @@
 // Two buttons, one with id of increment, other with decrement. both buttons will have a data-id equal to the pet ID, need to send a put request to the API endpoint with the score increment, the pet_id = to the data-id and the req.session.user_id in request
 
-const updateFormHandler = async (event) => {
+const incrementHandler = async (event) => {
   event.preventDefault();
 
-  const id = document.querySelector('#updateBtn').getAttribute('data-id');
-  const blog_name = document.querySelector('#blog-name').value.trim();
-  const blog_body = document.querySelector('#blog-body').value.trim();
+  const id = event.target.getAttribute('data-id');
+  console.log(id);
+  const pet_score = event.target.getAttribute('data-score');
+  console.log(pet_score);
 
-  if (blog_name && blog_body) {
-    const response = await fetch(`/api/score/`, {
-      method: 'put',
-      body: JSON.stringify({ blog_name, blog_body, id }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  const response = await fetch(`/api/inc/${id}`, {
+    method: 'put',
+    body: JSON.stringify({ pet_score }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to up vote');
-    }
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert('Failed to up vote');
   }
 };
 
-if (blog_name && blog_body) {
-    const response = await fetch(`/api/score/`, {
-      method: 'put',
-      body: JSON.stringify({ blog_name, blog_body, id }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+const decrementHandler = async (event) => {
+  event.preventDefault();
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to down vote');
-    }
+  const id = event.target.getAttribute('data-id');
+  const pet_score = event.target.getAttribute('#data-score');
+
+  console.log(id, pet_score);
+
+  const response = await fetch(`/api/score/`, {
+    method: 'put',
+    body: JSON.stringify({ id, pet_score }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/profile');
+  } else {
+    alert('Failed to down vote');
   }
 };
 
-document.querySelector('.pet_score').addEventListener('button', Pet);
+document
+  .querySelector('.increment')
+  .addEventListener('click', incrementHandler);
 
-document.querySelector('.pet_score').addEventListener('button', Pet);
+document
+  .querySelector('.decrement')
+  .addEventListener('click', decrementHandler);
