c00436c3c3633bf3162631a71915f41ad0eec507
kylegavinsimone <kylesimone@outlook.com>
2021-04-12 10:33:05 -0700

    random math for random image on gallery.js

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -2,10 +2,10 @@ const randomize = () => {
   // Gets up a number we can use for our randomizer
   const length = document.querySelector('#randomBtn').getAttribute('data-id');
 
-  const random = '';
   //Todo:create variable with math.random math.floor with a maximum = length var.
-
   //   document.location.replace(`/gallery/${random}`);
+  const random = Math.floor(Math.random() * array.length);
+  document.location.replace = `/gallery/${random}`;
 
   console.log("I'm here so James knows this is loading properly");
 };
