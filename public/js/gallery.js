af05afc9d0354401acb26256341c1179f4af1b0d
James V <james.vanhoke@yahoo.com>
2021-04-12 11:21:56 -0700

    Housekeeping

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,7 +1,6 @@
 const randomize = () => {
-  console.log("I work")
   const len = document.querySelector('#randomBtn').getAttribute('data-id');
-  
+
   const random = Math.floor(Math.random() * parseInt(len));
 
   document.location.replace(`/gallery/${random}`);
